8f695cc210b905d24165bf9d5831264752b77850
davidlyra <david.lyra@gmail.com>
2021-05-29 19:25:50 +0100

    Postman collection updated

--- src/main/java/com/library/api/controller/BookController.java
+++ src/main/java/com/library/api/controller/BookController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
